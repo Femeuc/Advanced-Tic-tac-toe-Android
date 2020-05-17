d51a626bd727636de6fa4be48feb127665644e5e
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-05-16 21:38:35 -0300

    Creates a class to save the socket created by the bluetooth connection.

---
+++ app/src/main/java/com/femeuc/advancedtic_tac_toe/SocketHandler.java
@@ -0,0 +1,15 @@
+package com.femeuc.advancedtic_tac_toe;
+
+import android.bluetooth.BluetoothSocket;
+
+public class SocketHandler {
+    private static BluetoothSocket socket;
+
+    public static synchronized BluetoothSocket getSocket(){
+        return socket;
+    }
+
+    public static synchronized void setSocket(BluetoothSocket socket){
+        SocketHandler.socket = socket;
+    }
+}
